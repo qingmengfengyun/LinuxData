822561c586f19c67a1d06eebaf47b25babb56ce6
Zavier <ZavierPei@proton.me>
2026-04-22 18:06:23 +0800

    python code update

---
+++ code/python/01_代码片段/test.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    print ("hello world")
+    pass
+
+if __name__ == "__main__":
+    main()
--- code/python/01_代码片段/私有方法.py
+++ code/python/01_代码片段/私有方法.py
@@ -1,5 +1,4 @@
 class Dog:
-
     # 私有方法
     def __send_msg(self):
         print("--------正在发生短信-----------------")
@@ -12,6 +11,10 @@ class Dog:
             print("余额不足，请先充值")
 
 
-dog = Dog()
-dog.send_msg(100000)
+def main():
+    dog = Dog()
+    dog.send_msg(100000)
 
+
+if __name__ == "__main__":
+    main()
